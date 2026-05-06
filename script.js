2f5ca2554a916e6a795ef495984c5697784d5404
arthur-programmerare <arthurnkr14@gmail.com>
2026-05-06 14:52:30 +0200

    js + html

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function raknaKraft() {
     let F = m * a;
 
     document.getElementById("resultat").innerText =
-        "Kraften är: " + F + " N";
+        // Visar både svaret och formeln som användes
+document.getElementById("resultat").innerHTML = 
+    `Kraften är: ${F} N(Beräknat via: ${m} kg * ${a} m/s²)`;
 }
 
 //Räkna Energi
@@ -36,3 +38,29 @@ function raknaEnergi() {
 }
 
 //formulär som är skyddad
+
+function visaNamn() {
+    let namn = document.getElementById("namn").value;
+
+    if (namn === "") {
+        document.getElementById("namnOutput").innerText = "Skriv ditt namn";
+        return;
+    }
+
+     namn = namn.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+       "Hej " + namn;
+       document.getElementById("namnOutput").innerText = "Hej " + namn;
+}
+function rensare() {
+    // Rensar alla input-rutor
+    document.getElementById("massa").value = "";
+    document.getElementById("acceleration").value = "";
+    document.getElementById("massaE").value = "";
+    document.getElementById("hastighet").value = "";
+    document.getElementById("namn").value = "";
+    
+    // Rensar texterna där resultaten stod
+    document.getElementById("resultat").innerText = "";
+    document.getElementById("resultatE").innerText = "";
+    document.getElementById("namnOutput").innerText = "";
+}
